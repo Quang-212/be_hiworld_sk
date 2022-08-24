9d504f6db86f0a7f92de65b8c60263921bf191f7
abeesdev <abeesdevjs@gmail.com>
2022-08-24 20:34:14 +0700

    Add chanel assignment-comment

--- src/models/assignment-comment.model.js
+++ src/models/assignment-comment.model.js
@@ -1,16 +1,27 @@
 // assignment-comment-model.js - A mongoose model
-// 
+//
 // See http://mongoosejs.com/docs/models.html
 // for more of what you can do here.
 module.exports = function (app) {
-  const modelName = 'assignmentComment';
-  const mongooseClient = app.get('mongooseClient');
+  const modelName = "assignmentComment";
+  const mongooseClient = app.get("mongooseClient");
   const { Schema } = mongooseClient;
-  const schema = new Schema({
-    text: { type: String, required: true }
-  }, {
-    timestamps: true
-  });
+  const schema = new Schema(
+    {
+      user: { type: Schema.Types.ObjectId, required: true, ref: "users" },
+      tag_id: { type: Schema.Types.ObjectId, default: null },
+      assignment_id: { type: Schema.Types.ObjectId, default: null },
+      comment: { type: String, required: true },
+      rating: { type: Number, default: null },
+      replyTo: {
+        type: Schema.Types.ObjectId,
+        default: null,
+      },
+    },
+    {
+      timestamps: true,
+    }
+  );
 
   // This is necessary to avoid model compilation errors in watch mode
   // see https://mongoosejs.com/docs/api/connection.html#connection_Connection-deleteModel
@@ -18,5 +29,4 @@ module.exports = function (app) {
     mongooseClient.deleteModel(modelName);
   }
   return mongooseClient.model(modelName, schema);
-  
 };
